cb7ecc9a640265c82f11d3b0fbe167620d99f22e
Martoncă Daniel <45790572+danielmartonca@users.noreply.github.com>
2022-05-06 10:34:30 +0300

    Created widget for custom list products TODO - fix setState

--- lib/view/custom_list_window.dart
+++ lib/view/custom_list_window.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:listonic_clone/model/listonic_list.dart';
-
-import '../database/boxes.dart';
-import '../database/lists.dart';
-import '../database/products.dart';
-import '../model/product.dart';
+import 'package:listonic_clone/view/custom_list_products_window.dart';
 
 class ListsBottomSheet extends StatefulWidget {
   final ListonicList currentList;
@@ -21,73 +16,11 @@ class ListsBottomSheet extends StatefulWidget {
 class _ListsBottomSheetState extends State<ListsBottomSheet> {
   bool toggleIcon = true;
   final double _addProductToListsIconSize = 50;
-  final _notSelectedColor = Colors.green;
-  final _selectedColor = Colors.amber;
-
-  late List<Product> _productsList;
-  late final Map<Product, Color> _selectedProductsMap =
-      Map<Product, Color>.identity();
 
   toggleIconState(bool value) {
     setState(() {
       toggleIcon = value;
     });
-  }
-
-  void addProductToCurrentList(ListonicList currentList, Product product) {
-    log.wtf(_selectedProductsMap[product]);
-    if (!currentList.products.contains(product)) {
-      setState(() {
-        _selectedProductsMap[product] = _selectedColor;
-        currentList.products.add(product);
-        // ScaffoldMessenger.of(context).showSnackBar(
-        //   SnackBar(
-        //       content: Text("Added '${product.name}' to the list."),
-        //       backgroundColor: Colors.green),
-        // );
-      });
-    } else {
-      setState(() {
-        _selectedProductsMap[product] = _notSelectedColor;
-        currentList.products.remove(product);
-        // ScaffoldMessenger.of(context).showSnackBar(
-        //   SnackBar(
-        //       content: Text("Removed '${product.name}' from the list."),
-        //       backgroundColor: Colors.grey),
-        // );
-      });
-    }
-    ListonicLists.updateList(currentList.name, currentList);
-  }
-
-  Widget createProductsListWidget() {
-    return Expanded(
-        child: ListView.builder(
-      shrinkWrap: true,
-      itemCount: _productsList.length,
-      // Provide a builder function. This is where the magic happens.
-      // Convert each item into a widget based on the type of item it is.
-      itemBuilder: (context, index) {
-        final Product product = _productsList[index];
-        Color? color = _selectedProductsMap[product];
-        color ??= _notSelectedColor;
-
-        return Container(
-          margin: const EdgeInsets.all(5.0),
-          padding: const EdgeInsets.all(10.0),
-          decoration: BoxDecoration(
-              color: color,
-              border:
-                  Border.all(color: Colors.black, style: BorderStyle.solid)),
-          child: ListTile(
-            dense: true,
-            title: product.buildTitle(context),
-            subtitle: product.buildSubtitle(context),
-            onTap: () => addProductToCurrentList(widget.currentList, product),
-          ),
-        );
-      },
-    ));
   }
 
   Widget getCurrentListProducts() {
@@ -109,62 +42,7 @@ class _ListsBottomSheetState extends State<ListsBottomSheet> {
       context: context,
       isScrollControlled: true,
       builder: (BuildContext context) {
-        return Padding(
-            padding: EdgeInsets.only(
-                bottom: MediaQuery.of(context).viewInsets.bottom),
-            child: Container(
-              height: 500,
-              color: Colors.lightBlue,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(top: 15),
-                      child: Text(
-                        'Select a product',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 15),
-                      child: Text(
-                        '(or add new one in Products)',
-                        style: TextStyle(
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                    FutureBuilder(
-                        future: Products.getProducts(),
-                        builder:
-                            (BuildContext context, AsyncSnapshot snapshot) {
-                          if (!snapshot.hasData) {
-                            return const Center(
-                                child: Padding(
-                              padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
-                              // child: Text("Loading data...")),
-                              child: SpinKitFadingCube(
-                                size: 30,
-                                color: Colors.green,
-                              ),
-                            ));
-                          } else {
-                            _productsList = snapshot.data;
-                            for (var product in _productsList) {
-                              _selectedProductsMap[product] = _notSelectedColor;
-                            }
-                            return createProductsListWidget();
-                          }
-                        })
-                  ],
-                ),
-              ),
-            ));
+        return CustomListProductsBottomSheet(widget.currentList,setState);
       },
     );
   }
